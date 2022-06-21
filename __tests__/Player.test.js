7f99b496455fcd0b63af787a16ce99a0c4fbd6e7
Booker King <btking1@my.waketech.edu>
2022-06-21 16:22:05 -0400

    finished player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,12 +14,60 @@ test("gets player's stats as an object", () => {
   expect(player.getStats()).toHaveProperty("agility");
 });
 
-test('gets inventory from player or returns false', () => {
-    const player = new Player('Soran');
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Soran");
 
-    expect(player.getInventory()).toEqual(expect.any(Array));
+  expect(player.getInventory()).toEqual(expect.any(Array));
 
-    player.inventory = [];
+  player.inventory = [];
 
-    expect(player.getInventory()).toEqual(false);
+  expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+  const player = new Player("Soran");
+
+  expect(player.getHealth()).toEqual(
+    expect.stringContaining(player.health.toString())
+  );
+});
+
+test("gets player's health value", () => {
+  const player = new Player("Soran");
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player("Soran");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
+
+test("gets player's attack value", () => {
+  const player = new Player("Soran");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("adds a potion to the inventory", () => {
+  const player = new Player("Soran");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
 });
